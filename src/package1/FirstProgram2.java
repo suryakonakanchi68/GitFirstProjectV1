48e4f4234483d514f4752911fc621c976679b893
surya <suryakonakanchi68@gmail.com>
2024-09-26 20:57:48 -0400

    Commiting to Branch ABC

---
+++ src/package1/FirstProgram2.java
@@ -0,0 +1,15 @@
+package package1;
+public class FirstProgram2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int q=10;
+		if(q>10) {
+		System.out.println("Greater than 10"); }
+		else {
+			System.out.println("Less than  or Equals to 10");  }
+		
+
+	}
+
+}
